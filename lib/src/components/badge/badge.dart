b8ac94bd83c59d24179e056ebe02a1c8aed60972
OverMiracle <13108302@qq.com>
2022-10-19 14:36:28 +0800

    20221017

--- lib/src/components/badge/badge.dart
+++ lib/src/components/badge/badge.dart
@@ -124,6 +124,7 @@ class DxBadge extends StatelessWidget {
   /// 构建内容
   Widget _buildContentBadge() {
     return Container(
+      height: themeData!.dotSize,
       constraints: BoxConstraints(minWidth: themeData!.size),
       alignment: Alignment.center,
       padding: themeData!.padding,
--- lib/src/theme/components/badge/badge.dart
+++ lib/src/theme/components/badge/badge.dart
@@ -62,7 +62,6 @@ class DxBadgeThemeData {
     double? borderWidth,
     Color? backgroundColor,
     Color? dotColor,
-    double? dotSize,
     String? fontFamily,
   }) {
     return DxBadgeThemeData.raw(
@@ -74,7 +73,6 @@ class DxBadgeThemeData {
       borderWidth: borderWidth ?? DxStyle.borderWidthBase,
       backgroundColor: backgroundColor ?? DxStyle.red,
       dotColor: dotColor ?? DxStyle.red,
-      dotSize: dotSize ?? 8.0,
       fontFamily: fontFamily ?? '',
     );
   }
@@ -88,7 +86,6 @@ class DxBadgeThemeData {
     required this.borderWidth,
     required this.backgroundColor,
     required this.dotColor,
-    required this.dotSize,
     required this.fontFamily,
   });
 
@@ -101,7 +98,6 @@ class DxBadgeThemeData {
     double? borderWidth,
     Color? backgroundColor,
     Color? dotColor,
-    double? dotSize,
     String? fontFamily,
   }) {
     return DxBadgeThemeData(
@@ -113,7 +109,6 @@ class DxBadgeThemeData {
       borderWidth: borderWidth ?? this.borderWidth,
       backgroundColor: backgroundColor ?? this.backgroundColor,
       dotColor: dotColor ?? this.dotColor,
-      dotSize: dotSize ?? this.dotSize,
       fontFamily: fontFamily ?? this.fontFamily,
     );
   }
@@ -131,7 +126,6 @@ class DxBadgeThemeData {
       borderWidth: other.borderWidth,
       backgroundColor: other.backgroundColor,
       dotColor: other.dotColor,
-      dotSize: other.dotSize,
       fontFamily: other.fontFamily,
     );
   }
@@ -146,7 +140,6 @@ class DxBadgeThemeData {
       borderWidth: lerpDouble(a?.borderWidth, b?.borderWidth, t),
       backgroundColor: Color.lerp(a?.backgroundColor, b?.backgroundColor, t),
       dotColor: Color.lerp(a?.dotColor, b?.dotColor, t),
-      dotSize: lerpDouble(a?.dotSize, b?.dotSize, t),
       fontFamily: b?.fontFamily,
     );
   }
